7051817fdd6cdb30fb2b7e298978883337b60858
IsaKatha98 <isabel.loerzer@iesnervion.es>
2023-01-16 18:52:25 +0100

    C2

--- HolaMundo/src/holamundo/HolaMundo.java
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -4,7 +4,7 @@ public class HolaMundo {
 
 	public static void main(String[] args) {
 		
-		System.out.println("hola, mundo");
+		System.out.println("hola, mundo. Me llamo Isa y he creado una rama nueva.");
 	}
 
 }
